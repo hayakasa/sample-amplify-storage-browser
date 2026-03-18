d01b3af686b11eec233b9325d7b3741f16600116
Ryoji Hayasaka <hayakasa@gmail.com>
2026-03-18 11:30:32 +0900

    Rename export to addUserToGroup
    Rename the exported constant from `addUserGroup` to `addUserToGroup` to match the resource/file name (`add-user-to-group`) and fix a naming inconsistency.

--- amplify/data/add-user-to-group/resource.ts
+++ amplify/data/add-user-to-group/resource.ts
@@ -1,5 +1,5 @@
 import { defineFunction } from "@aws-amplify/backend"
 
-export const addUserGroup = defineFunction({
+export const addUserToGroup = defineFunction({
     name: "add-user-to-group"
 })
