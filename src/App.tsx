45efa446a2af4582e7f8de9266e836f1e10eee94
Ryoji Hayasaka <hayakasa@gmail.com>
2026-03-25 17:28:08 +0900

    tab ui test

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,31 @@ import './App.css';
 
 import config from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
-import { Authenticator, Button } from '@aws-amplify/ui-react';
+import { Authenticator, Button, Link, Flex, Heading } from '@aws-amplify/ui-react';
+import {
+  BrowserRouter as Router,
+  Link as ReactRouterLink,
+  Routes,
+  Route
+} from 'react-router-dom'
+
 Amplify.configure(config);
 
 const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
 });
+
+function Home() {
+  return <Heading level={2}>Home</Heading>;
+}
+
+function About() {
+  return <Heading level={2}>About</Heading>;
+}
+
+function Users() {
+  return <Heading level={2}>Users</Heading>;
+}
 
 function App() {
   return (
@@ -22,6 +41,19 @@ function App() {
           <div className="header">
             <h1>{`Hello ${user?.username}`}</h1>
             <Button onClick={signOut}>Sign out</Button>
+            <Router>
+              <Flex>
+                <ReactRouterLink to="/">Home</ReactRouterLink>
+                <ReactRouterLink to="/about">About</ReactRouterLink>
+                <ReactRouterLink to="/users">Users</ReactRouterLink>
+              </Flex>
+
+              <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </Router>
           </div>
           <StorageBrowser />
         </>
