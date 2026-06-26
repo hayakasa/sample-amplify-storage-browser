dc07d861337a95da25bd28a7cc8227c0786d7be0
Ryoji Hayasaka <hayakasa@gmail.com>
2026-06-26 13:16:51 +0900

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,84 +1,84 @@
 import { defineBackend } from '@aws-amplify/backend'
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
 import * as s3 from 'aws-cdk-lib/aws-s3'
-// import { auth } from './auth/resource'
+import { auth } from './auth/resource'
 import { data } from './data/resource'
 
 import { storage } from './storage/resource'
-// import { adminCreateUser } from './data/admin-create-user/resource'
-// import { adminUpdateUser } from './data/admin-update-user/resource'
-// import { listUsers } from './data/list-users/resource'
-// import { adminDeleteUser } from './data/admin-delete-user/resource'
-// import { adminToggleSuspend } from './data/admin-toggle-suspend/resource'
+import { adminCreateUser } from './data/admin-create-user/resource'
+import { adminUpdateUser } from './data/admin-update-user/resource'
+import { listUsers } from './data/list-users/resource'
+import { adminDeleteUser } from './data/admin-delete-user/resource'
+import { adminToggleSuspend } from './data/admin-toggle-suspend/resource'
 
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
-  // auth,
+  auth,
   data,
   storage,
-  // listUsers,
-  // adminCreateUser,
-  // adminUpdateUser,
-  // adminDeleteUser,
-  // adminToggleSuspend,
+  listUsers,
+  adminCreateUser,
+  adminUpdateUser,
+  adminDeleteUser,
+  adminToggleSuspend,
 })
 
 // Lambda関数にCognito操作権限を付与
-// const authenticatedUserRole = backend.auth.resources.authenticatedUserIamRole
-// const userPool = backend.auth.resources.userPool
+const authenticatedUserRole = backend.auth.resources.authenticatedUserIamRole
+const userPool = backend.auth.resources.userPool
 
-// backend.listUsers.resources.lambda.addToRolePolicy(
-//   new PolicyStatement({
-//     actions: [
-//       'cognito-idp:AdminListGroupsForUser',
-//       'cognito-idp:ListUsers',
-//     ],
-//     resources: [userPool.userPoolArn],
-//   })
-// )
-// backend.listUsers.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
+backend.listUsers.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: [
+      'cognito-idp:AdminListGroupsForUser',
+      'cognito-idp:ListUsers',
+    ],
+    resources: [userPool.userPoolArn],
+  })
+)
+backend.listUsers.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
 
-// backend.adminCreateUser.resources.lambda.addToRolePolicy(
-//   new PolicyStatement({
-//     actions: ['cognito-idp:AdminCreateUser'],
-//     resources: [userPool.userPoolArn],
-//   })
-// )
-// backend.adminCreateUser.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
+backend.adminCreateUser.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: ['cognito-idp:AdminCreateUser'],
+    resources: [userPool.userPoolArn],
+  })
+)
+backend.adminCreateUser.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
 
-// backend.adminUpdateUser.resources.lambda.addToRolePolicy(
-//   new PolicyStatement({
-//     actions: ['cognito-idp:AdminUpdateUserAttributes'],
-//     resources: [userPool.userPoolArn],
-//   })
-// )
-// backend.adminUpdateUser.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
+backend.adminUpdateUser.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: ['cognito-idp:AdminUpdateUserAttributes'],
+    resources: [userPool.userPoolArn],
+  })
+)
+backend.adminUpdateUser.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
 
-// backend.adminDeleteUser.resources.lambda.addToRolePolicy(
-//   new PolicyStatement({
-//     actions: [
-//       'cognito-idp:AdminListGroupsForUser',
-//       'cognito-idp:AdminDeleteUser',
-//     ],
-//     resources: [userPool.userPoolArn],
-//   })
-// )
-// backend.adminDeleteUser.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
+backend.adminDeleteUser.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: [
+      'cognito-idp:AdminListGroupsForUser',
+      'cognito-idp:AdminDeleteUser',
+    ],
+    resources: [userPool.userPoolArn],
+  })
+)
+backend.adminDeleteUser.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
 
-// backend.adminToggleSuspend.resources.lambda.addToRolePolicy(
-//   new PolicyStatement({
-//     actions: [
-//       'cognito-idp:AdminGetUser',
-//       'cognito-idp:AdminDisableUser',
-//       'cognito-idp:AdminEnableUser',
-//     ],
-//     resources: [userPool.userPoolArn],
-//   })
-// )
-// backend.adminToggleSuspend.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
+backend.adminToggleSuspend.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: [
+      'cognito-idp:AdminGetUser',
+      'cognito-idp:AdminDisableUser',
+      'cognito-idp:AdminEnableUser',
+    ],
+    resources: [userPool.userPoolArn],
+  })
+)
+backend.adminToggleSuspend.addEnvironment('AMPLIFY_AUTH_USER_POOL_ID', userPool.userPoolId)
 
 // Bucker内のオブジェクトを保持する期間を設定（7日間、バックアップなし）
 const s3Bucket = backend.storage.resources.bucket
